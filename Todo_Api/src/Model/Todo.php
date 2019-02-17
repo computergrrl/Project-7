3ee4ae7fc4de02b3406b4e7c4955398212323c4a
computergrrl <44240149+computergrrl@users.noreply.github.com>
2019-02-17 00:47:36 -0800

    CRUD functions added to Todo class

--- Todo_Api/src/Model/Todo.php
+++ Todo_Api/src/Model/Todo.php
@@ -12,11 +12,54 @@ class Todo
 
    }
 
-   public function getTodos ()
+   public function getTasks ()
    {
      $sql = $this->database->prepare('SELECT * FROM tasks');
      $sql->execute();
      return $sql->fetchAll();
+
+   }
+
+   public function getTask ($task_id)
+   {
+     $sql = $this->database->prepare('SELECT * FROM tasks WHERE id = :id');
+     $sql->bindParam('id' , $task_id);
+     $sql->execute();
+     return $sql->fetch();
+
+   }
+
+
+   public function createTask($data)
+   {
+      $sql = $this->database->prepare('INSERT INTO tasks (task, status) VALUES (:task, :status)');
+      $sql->bindParam('task' , $data['task']);
+      $sql->bindParam('status' , $data['status']);
+      $sql->execute();
+      return $this->getTask($this->database->lastInsertId());
+
+
+   }
+
+   public function updateTask($data)
+   {
+      $sql = $this->database->prepare('UPDATE tasks SET task = :task, status = :status WHERE id = :id');
+      $sql->bindParam('task' , $data['task']);
+      $sql->bindParam('status' , $data['status']);
+      $sql->bindParam('id' , $data['id']);
+      $sql->execute();
+      return $this->getTask($data['id']);
+
+
+   }
+
+   public function deleteTask($task_id)
+   {
+      $sql = $this->database->prepare('DELETE FROM tasks WHERE id = :id');
+      $sql->bindParam('id' , $data['id']);
+      $sql->execute();
+      return ['message' => 'The course was deleted successfully'];
+  
 
    }
 
