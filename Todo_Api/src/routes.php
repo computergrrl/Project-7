19f1e436d9205e075781a191898c5c21b5ef4257
computergrrl <44240149+computergrrl@users.noreply.github.com>
2019-02-17 12:30:06 -0800

    Updated routes and route args to correct format per project instructions

--- Todo_Api/src/routes.php
+++ Todo_Api/src/routes.php
@@ -24,15 +24,17 @@ $app->get('/', function (Request $request, Response $response, array $args) {
      return $response->withJson($result, 200, JSON_PRETTY_PRINT );
 });
 /*************************create a routing group  ****************************/
-$app->group('/api/v1/tasks', function() use($app) {
+$app->group('/api/v1/todos', function() use($app) {
+
+        /*************************Get all tasks ****************************/
         $app->get('', function (Request $request, Response $response, array $args) {
             $result = $this->todo->getTasks();
             return $response->withJson($result, 200, JSON_PRETTY_PRINT);
         });
 
-        /*************************Get a task ****************************/
-        $app->get('/{task_id}', function (Request $request, Response $response, array $args) {
-            $result = $this->todo->getTask($args['task_id']);
+        /**********************Get a single task ************************/
+        $app->get('/{id}', function (Request $request, Response $response, array $args) {
+            $result = $this->todo->getTask($args['id']);
 
             return $response->withJson($result, 200, JSON_PRETTY_PRINT);
         });
@@ -41,19 +43,19 @@ $app->group('/api/v1/tasks', function() use($app) {
 
             $result = $this->todo->createTask($request->getParsedBody());
 
-            return $response->withJson($result, 200, JSON_PRETTY_PRINT);
+            return $response->withJson($result, 201, JSON_PRETTY_PRINT);
         });
         /*************************Update task ****************************/
-        $app->put('/{task_id}', function (Request $request, Response $response, array $args) {
+        $app->put('/{id}', function (Request $request, Response $response, array $args) {
             $data = $request->getParsedBody();
-            $data['task_id'] = $args['task_id'];
+            $data['id'] = $args['id'];
             $result = $this->todo->updateTask($data);
 
             return $response->withJson($result, 200, JSON_PRETTY_PRINT);
         });
         /*************************Delete task ****************************/
-        $app->delete('/{task_id}', function (Request $request, Response $response, array $args) {
-            $result = $this->todo->deleteTask($args['task_id']);
+        $app->delete('/{id}', function (Request $request, Response $response, array $args) {
+            $result = $this->todo->deleteTask($args['id']);
 
             return $response->withJson($result, 200, JSON_PRETTY_PRINT);
         });
